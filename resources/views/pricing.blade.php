eb5d5d4828cd05790452865e5f77c61d2d7bb665
Nawathpolkaw <nawathpolkaw@gmail.com>
2021-08-21 11:21:14 +0700

    add

---
+++ resources/views/pricing.blade.php
@@ -0,0 +1,89 @@
+<!-- End of section -->
+<section class="section bg-white">
+    <div class="container">
+        <div class="mb-4 mb-lg-5 text-center">
+            <h2 class="font-weight-light">Are you ready for Leaf?</h2>
+        </div>
+        <div class="row d-flex justify-content-center">
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="pricing-card">
+                    <div class="card shadow-sm border-soft mt-1">
+                        <!-- Header -->
+                        <header class="card-header bg-white p-3 text-center">
+                            <h2 class="h5 text-primary mb-4">Freelancer License</h2>
+                            <span class="d-block">
+                                <span class="display-1 text-dark font-weight-bold">
+                                    <span class="align-top font-medium">$</span>59
+                                </span>
+                            </span>
+                        </header>
+                        <!-- End Header -->
+                        <!-- Content -->
+                        <div class="card-body">
+                            <ul class="list-group mb-4">
+                                <li class="list-group-item"><i class="fas fa-check-circle mr-1 text-success"></i> Single
+                                    website for client</li>
+                                <li class="list-group-item"><i class="fas fa-check-circle mr-1 text-success"></i> Single
+                                    personal website</li>
+                                <li class="list-group-item"><i class="fas fa-check-circle mr-1 text-success"></i> 6
+                                    months friendly support</li>
+                                <li class="list-group-item"><i class="fas fa-check-circle mr-1 text-success"></i> Future
+                                    updates</li>
+                                <li class="list-group-item"><i class="fas fa-check-circle mr-1 text-success"></i> Money
+                                    back guarantee</li>
+                                <li class="list-group-item"><i class="fas fa-times-circle mr-1 text-danger"></i>
+                                    Multiple personal websites</li>
+                                <li class="list-group-item"><i class="fas fa-times-circle mr-1 text-danger"></i>
+                                    Multiple websites for clients</li>
+                            </ul>
+                            <a href="https://themesberg.com/product/bootstrap/leaf-non-profit-environmental-bootstrap-4-theme"
+                                class="btn btn-primary btn-block animate-up-2" target="_blank">Buy Freelancer License</a>
+                        </div>
+                        <!-- End Content -->
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+                <div class="pricing-card">
+                    <div class="card shadow-sm border-soft mt-1">
+                        <!-- Header -->
+                        <header class="card-header bg-white p-3 text-center">
+                            <h2 class="h5 text-secondary mb-4">Company License</h2>
+                            <span class="d-block">
+                                <span class="display-1 text-dark font-weight-bold">
+                                    <span class="align-top font-medium">$</span>99
+                                </span>
+                            </span>
+                        </header>
+                        <!-- End Header -->
+                        <!-- Content -->
+                        <div class="card-body">
+                            <ul class="list-group mb-4">
+                                <li class="list-group-item"><i class="fas fa-check-circle mr-1 text-secondary"></i>
+                                    Single website for client</li>
+                                <li class="list-group-item"><i class="fas fa-check-circle mr-1 text-secondary"></i>
+                                    Single personal website</li>
+                                <li class="list-group-item"><i class="fas fa-check-circle mr-1 text-secondary"></i> 6
+                                    months friendly support</li>
+                                <li class="list-group-item"><i class="fas fa-check-circle mr-1 text-secondary"></i>
+                                    Future updates</li>
+                                <li class="list-group-item"><i class="fas fa-check-circle mr-1 text-secondary"></i>
+                                    Money back guarantee</li>
+                                <li class="list-group-item"><i class="fas fa-check-circle mr-1 text-secondary"></i>
+                                    Multiple personal websites</li>
+                                <li class="list-group-item"><i class="fas fa-check-circle mr-1 text-secondary"></i>
+                                    Multiple websites for clients</li>
+                            </ul>
+                            <a href="https://themesberg.com/product/bootstrap/leaf-non-profit-environmental-bootstrap-4-theme"
+                                class="btn btn-outline-secondary btn-block" target="_blank">Buy Company License</a>
+                        </div>
+                        <!-- End Content -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <p class="text-center mt-4"><a href="https://themesberg.com/licensing" target="_blank"
+                class="text-gray font-small font-weight-light"><i class="fas fa-question-circle mr-1"></i> Read more
+                about licensing</a></p>
+    </div>
+</section>
---
+++ resources/views/scripts.blade.php
@@ -0,0 +1,25 @@
+<!-- Core -->
+<script src="@@path/node_modules/jquery/dist/jquery.min.js"></script>
+<script src="@@path/node_modules/popper.js/dist/umd/popper.min.js"></script>
+<script src="@@path/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="@@path/node_modules/headroom.js/dist/headroom.min.js"></script>
+
+<!-- Vendor JS -->
+<script src="@@path/node_modules/onscreen/dist/on-screen.umd.min.js"></script>
+<script src="@@path/node_modules/nouislider/distribute/nouislider.min.js"></script>
+<script src="@@path/node_modules/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+<script src="@@path/node_modules/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js"></script>
+<script src="@@path/node_modules/waypoints/lib/jquery.waypoints.min.js"></script>
+<script src="@@path/node_modules/owl.carousel/dist/owl.carousel.min.js"></script>
+<script src="@@path/node_modules/jarallax/dist/jarallax.min.js"></script>
+<script src="@@path/node_modules/jquery.counterup/jquery.counterup.min.js"></script>
+<script src="@@path/node_modules/jquery-countdown/dist/jquery.countdown.min.js"></script>
+<script src="@@path/node_modules/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
+<script src="@@path/node_modules/chartist/dist/chartist.js"></script>
+<script src="@@path/node_modules/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script>
+<script src="@@path/node_modules/prismjs/prism.js"></script>
+<script src="@@path/node_modules/jqvmap/dist/jquery.vmap.min.js"></script>
+<script src="@@path/node_modules/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+
+<!-- Leaf JS -->
+<script src="@@path/assets/js/leaf.js"></script>
